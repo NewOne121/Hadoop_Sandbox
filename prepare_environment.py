51468c5bd29f7d01c5e538dcd93c04e5602ed7cd
Filipp Filippov <f.filippov7@gmail.com>
2019-05-02 22:55:52 +0300

    pre-alpha

--- prepare_environment.py
+++ prepare_environment.py
@@ -3,6 +3,18 @@
 import yum
 import subprocess
 import os
+import sys
+import urllib
+import tarfile
+import argparse
+
+parser=argparse.ArgumentParser()
+parser.add_argument('--role', action='store', help='Node role. Valid roles are: namenode, resource-manager, datanode')
+args=parser.parse_args()
+
+if not args.role:
+  parser.print_help(sys.stderr)
+  sys.exit(1)
 
 def install_packages():
   to_install = ['java-1.8.0-openjdk']
@@ -20,25 +32,64 @@ def install_packages():
       yum_communicate.buildTransaction()
       yum_communicate.processTransaction()
 
+
+#Shell communicator
 def exec_shell(command):
   return os.popen(command).read().strip('\n')
 
-
-#Set java homedir in bash_profile
 homeDir = exec_shell('echo $HOME')
-profileFile = homeDir + '/.bash_profile'
+bashProfile = homeDir + '/.bash_profile'
+etcProfile = '/etc/profile'
 javaHome = os.readlink('/etc/alternatives/java')
-javaVar = 'export JAVA_HOME = \'' + javaHome + '\'\n'
-isvar='0'
-with open(profileFile, "a+") as bashpr:
-  for line in bashpr:
-    if javaVar in line:
-      isvar = '1'
-  if isvar == '0':
+javaVar = 'export JAVA_HOME=\'' + javaHome + '\'\n'
+bashrcVar = 'source ' + homeDir + '/.bashrc'
+
+def setjava():
+  with open(bashProfile, "a+") as bashpr:
     bashpr.write(javaVar)
+    print 'Java home is set.'
 
+#Check if JAVA_HOME is already set
+isvar='0'
+files = [ bashRc, bashProfile, etcProfile ]
+for file in files:
+  with open(file, 'r') as bashfile:
+    for line in bashfile:
+      if javaVar in line:
+        isvar = '1'
+        print 'JAVA_HOME already set'
 
+def getHadoop():
+  global destinationFile
+  global hadoopVersion
+  hadoopVersion = '3.1.2'
+  packageUrl = 'http://apache-mirror.rbc.ru/pub/apache/hadoop/common/hadoop-' + hadoopVersion + '/hadoop-' + hadoopVersion + '.tar.gz'
+  destinationFile = '/tmp/hadoop' + hadoopVersion + '.tar.gz'
+  url_comm = urllib.URLopener()
+  url_comm.retrieve(packageUrl, destinationFile)
 
+def extractHadoop():
+  tar = tarfile.open(destinationFile)
+  tar.extractall(path='/opt')
+  tar.close()
+
+def deployHadoopConfig(role): #from args
+  hadoopConfDir = '/opt/hadoop-' + hadoopVersion '/etc/hadoop'
+  commConfDir = './config/common/'
+  roleConfDir = './config/' + role + '/'
+  for item in commConfDir,roleConfDir:
+    for root, dirs, files in os.walk(item):
+      files.remove('hosts')
+      HadoopConf = files
+    for conf in HadoopConf:
+      exec_shell('cp', '\'' + commConfDir + conf + '\'', hadoopConfDir)
+  exec_shell(['cp', './config/common/hosts', '/etc/hosts'])
+    
 
 install_packages()
+if isvar == '0': setjava()
+getHadoop()
+extractHadoop()
+deployHadoopConfig()
+
 print 'Dont forget to:\nsource ~/.bash_profile'
